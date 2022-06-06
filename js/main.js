1dd26d2e9451ac45aa3a1d31553fe89f810a0dd3
Christik <christik.lucid@gmail.com>
2022-06-06 20:20:29 +0300

    Refactor getRandomNumber()

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 const getRandomNumber = (from, to) => {
-  const isParamsInvalid = (from < 0) || (to < 0) || (from === to);
-
-  if (isParamsInvalid) {
-    return null;
+  if (from < 0 || from > to) {
+    throw new Error('Диапазон может быть только положительный, а значение «до» меньше, чем значение «от»');
   }
 
-  const [ rangeStart, rangeEnd ] = (from < to) ? [from, to] : [to, from];
-  const randomNumber = Math.random() * (rangeEnd - rangeStart + 1) + rangeStart;
+  const randomNumber = Math.random() * (to - from + 1) + from;
   const randomInteger = Math.floor(randomNumber);
 
   return randomInteger;
