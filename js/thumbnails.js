4bb72dd5c89f7ab0dad3e29818d67de05b075446
Christik <christik.lucid@gmail.com>
2022-06-22 16:55:19 +0300

    Render thumbnails

---
+++ js/thumbnails.js
@@ -0,0 +1,23 @@
+const thumbnailTemplateElement = document.querySelector('#picture').content.querySelector('.picture');
+const thumbnailsContainerElement = document.querySelector('.pictures');
+const fragment = document.createDocumentFragment();
+
+const renderThumbnails = (posts) => {
+  posts.forEach((post) => {
+    const thumbnailElement = thumbnailTemplateElement.cloneNode(true);
+    const imageElement = thumbnailElement.querySelector('.picture__img');
+    const likesElement = thumbnailElement.querySelector('.picture__likes');
+    const commentsElement = thumbnailElement.querySelector('.picture__comments');
+
+    imageElement.src = post.url;
+    imageElement.alt = post.description;
+    likesElement.textContent = post.likes;
+    commentsElement.textContent = post.comments.length;
+
+    fragment.append(thumbnailElement);
+  });
+
+  thumbnailsContainerElement.append(fragment);
+};
+
+export {renderThumbnails};
